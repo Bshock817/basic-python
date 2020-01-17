cf7de9cc4daefd3d2508aaf49c4503d06635985f
Brian Shockency <53105161+Bshock817@users.noreply.github.com>
2020-01-16 19:58:31 -0500

    Add files via upload

---
+++ Loop1.py
@@ -0,0 +1,31 @@
+#Basic
+#for x in range(151):
+#    print(x)
+#Multiples of five
+#for x in range(0,1000,5):
+#    print(x)
+"""
+for x in range(1,31,1):
+    if x % 5 == 0:
+        print("Coding")
+    if x % 10 == 0:
+        print("Coding Dojo")
+    print(x)
+"""
+"""
+for x in range(0,500000,1):
+    sum = x
+    if x % 2 == 1:
+        sum = sum + x
+    print(sum)
+"""
+#for x in range(2018,0,-4):
+#   print(x)
+"""
+for x in range(15):
+    lowNum = 2
+    highNum = 10
+    mult = 2
+    if x % 2 == 0:
+        print(x)
+"""
